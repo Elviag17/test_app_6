a96e9de1b49df70a63e632ba062827fe29eda040
Elvia Gutierrez <elviagutierrez07@gmail.com>
2021-02-02 00:11:20 -0800

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # add root route and specify which controller to send to, and then which method to use 
   root 'pages#home'
-
+  # get request, page you want to capture, sent to pages controller, and use the about method
+  get 'about', to:'pages#about'
   
 
 end
